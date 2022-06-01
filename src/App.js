92a618ab58fefff036d940828c8693b6ea15902f
CJOcode <cjoconnor24@gmail.com>
2022-05-31 18:57:08 -0700

    got buttons to work

--- src/App.js
+++ src/App.js
@@ -6,28 +6,55 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: [true, true, true, true, true, true, true, true, true],
+      squares: [null, null, null, null, null, null, null, null, null],
+      turn: 1,
       xo: ["X", "O"]
     }
   }
-//   componentDidMount() {
-//     this.setState({squares: true}) 
-// }
 
-
-// handleGamePlay = (index) => {
-//     const { squares } = this.state
-//     if (squares[index] === true) {
-//       squares[index] = "X"
-//       this.setState({squares: false})
-//     } else if (squares[index] === false) {
-//       squares[index] = "O"
-//       this.setState({squares: true})
-//     }
-//   }
+    calculateWinner = (squares) => {
+    const {turn} = this.state
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        if (turn % 2 === 0) {
+        alert("YAY PLAYER X YOU WON");
+      } else  {
+        alert("YAY PLAYER O YOU WON");
+      }
+    }   
+  }
+  return null;
+}
 
 handleGamePlay = (index) => {
-  alert(index)}
+  const {squares, turn} = this.state
+  if( squares[index] !== null) {
+    void(0)
+  }
+  else if (turn % 2 === 0 ){
+    squares[index] = "X"
+    this.calculateWinner(squares)
+    this.setState({squares: squares, turn: turn + 1})
+  }
+  else if (turn % 2 !== 0 ) {
+    squares[index] = "O"
+    this.calculateWinner(squares)
+    this.setState({squares: squares, turn: turn + 1})
+    
+  }
+
+}
 
   render() {
     console.log(this.state.squares)
